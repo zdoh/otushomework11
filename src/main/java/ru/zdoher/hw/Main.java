f38c0c921aa747479f4e3aba6d1798eaa5718f3c
zdoh <denfairycat@gmail.com>
2019-03-04 17:36:48 +0300

    change csv data file to japanese words

--- src/main/java/ru/zdoher/hw/Main.java
+++ src/main/java/ru/zdoher/hw/Main.java
@@ -3,11 +3,9 @@ package ru.zdoher.hw;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import ru.zdoher.hw.view.TestingImpl;
 
-import java.io.IOException;
-
 public class Main {
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("/spring-context.xml");
 
         TestingImpl testingService = context.getBean(TestingImpl.class);
